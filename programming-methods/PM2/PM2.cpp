c206b9749c280b045fed26b128b18578c1881e20
Eclipthyx <36221645+Eclipthyx@users.noreply.github.com>
2020-09-24 21:54:23 +0200

    Update PM2.cpp

--- programming-methods/PM2/PM2.cpp
+++ programming-methods/PM2/PM2.cpp
@@ -1,52 +1,68 @@
 #include <string>
 #include <fstream>
 #include <iostream>
+#include <climits>
 using namespace std;
  
 //Programma van Yvo Hu en Wietske ...
 //Laatste update 22-9-2020
-//Gebruike compiler GNU g++
-void split_getal(int i,ofstream& uitvoer)
-{
-    i > 9 ? split_getal(i / 10,uitvoer) : void();
+//Gebruikte compiler GNU g++
+void split_getal(int,ofstream&);
+void encode(ifstream&, ofstream&);
 
-    int getal = i % 10;
-    uitvoer.put('0' + getal);
-}
-
-void encode(ifstream& invoer,ofstream& uitvoer)
-{
+void is_lychrel_getal(string invoernaam){
+    ifstream invoer(invoernaam,ios::in);
     char letter = invoer.get();
-    char vorige_letter = letter;
     while(!invoer.eof()){
         int count = 0;
-        while(letter == vorige_letter){
-            count++;
-            letter = invoer.get();
-        }
-        if(vorige_letter == '\\'){
-            uitvoer.put('\\');
-            uitvoer.put('\\');
-        }
-        else if(vorige_letter >= int('0') && vorige_letter <= int('9')){
-            uitvoer.put('\\');
-            uitvoer.put(vorige_letter);
-        }
-        else if(vorige_letter == '\n'){
-            for(int i = 0; i < count; i++){
-                uitvoer.put(vorige_letter);
+        int getal[10] = {0,0,0,0,0,0,0,0,0,0};
+        if(isdigit(letter)){
+            while(isdigit(letter)){
+                getal[count] = letter - '0';
+                std::cout << getal[count];
+                count++;
+                letter = invoer.get();
             }
+            cout << endl;
         }
-        else{
-            uitvoer.put(vorige_letter);
-        }
-        if(count > 1 && vorige_letter != '\n'){
-            split_getal(count,uitvoer);
-        }
-        vorige_letter = letter;
+
+        //process result
+        //reset
+        letter = invoer.get();
     }
-    uitvoer.close();
+    invoer.close();
 }
+
+/*
+long long int numReverse(long long int number)
+{
+    long long int rem = 0;
+    while (number > 0)
+    {
+    rem = (rem * 10) + (number % 10);
+        number = number / 10;
+    }
+    return rem;
+}
+bool is_Palindrome(long long int num)
+{
+    return (num == numReverse(num));
+}
+bool isLychrel(int num, const int iterCount = 500)
+{
+    long long int temp = num;
+    long long int rev;
+    for (int i = 0; i < iterCount; i++)
+    {
+        rev = numReverse(temp);
+        if (is_Palindrome(rev + temp))
+            return false;
+        temp = temp + rev;
+    }
+    return true;
+}
+*/
+
 int main()
 {
     ifstream invoer;
@@ -59,7 +75,7 @@ int main()
     int flag_keuze = 1;
     cout << "Wil je coderen(c) of decoderen?(d)" << endl;
     do{
-        cin >> keuze;
+        keuze = cin.get();
         if(keuze == 'C' || keuze == 'c'){
             flag_keuze = 0;
         }
@@ -83,20 +99,59 @@ int main()
         }
     } while(flag_invoer);
 
-    do{
-        cout << "Wat is de naam van het uitvoerbestand?" << endl;
-        cin >> uitvoernaam;
-        uitvoer.open(uitvoernaam,ios::out);
-        if(uitvoer.fail()){
-            cerr << "Het openen van " << uitvoernaam << " is mislukt. Probeer het nogmaals" << endl;
-        }
-        else{
-            flag_uitvoer = 0;
-        }
-    } while(flag_uitvoer);
+    //
+    cout << "Wat is de naam van het uitvoerbestand?" << endl;
+    cin >> uitvoernaam;
+    uitvoer.open(uitvoernaam,ios::out);
 
     encode(invoer,uitvoer);
+    invoer.close();
+    uitvoer.close();
+
+    is_lychrel_getal(invoernaam);
 
     system("pause");
     return 0;
+}
+
+void split_getal(int i,ofstream& uitvoer)
+{
+    i > 9 ? split_getal(i / 10,uitvoer) : void();
+
+    int getal = i % 10;
+    uitvoer.put('0' + getal);
+}
+
+void encode(ifstream& invoer,ofstream& uitvoer)
+{
+    char letter = invoer.get();
+    char vorige_letter = letter;
+    while(!invoer.eof()){
+        int count = 0;
+        while(letter == vorige_letter){
+            count++;
+            letter = invoer.get();
+        }
+        if(vorige_letter == '\\'){
+            uitvoer.put('\\');
+            uitvoer.put('\\');
+        }
+        else if(isdigit(vorige_letter)){
+            uitvoer.put('\\');
+            uitvoer.put(vorige_letter);
+        }
+        else if(vorige_letter == '\n'){
+            for(int i = 0; i < count; i++){
+                uitvoer.put(vorige_letter);
+            }
+        }
+        else{
+            uitvoer.put(vorige_letter);
+        }
+        if(count > 1 && vorige_letter != '\n'){
+            split_getal(count,uitvoer);
+        }
+        vorige_letter = letter;
+    }
+    uitvoer.close();
 }
