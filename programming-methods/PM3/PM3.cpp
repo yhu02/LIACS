8197800067214a988e7130967fbf99dc156627b6
Eclipthyx <36221645+Eclipthyx@users.noreply.github.com>
2020-10-27 12:21:00 +0100

    Update PM3.cpp

--- programming-methods/PM3/PM3.cpp
+++ programming-methods/PM3/PM3.cpp
@@ -23,11 +23,22 @@ class nonogram{
         static void stelPercentage();
 
         static void hoofdMenu();
+
+        static const int MAX = 50; //-----------------added W
+        static void maakNonoArray();
+        static void drukAf();
+
     private:
         static int grootte[2];
         static char symbool[2];
         static int cursor;
         static int rpercent;
+
+         //---------------added W
+        static bool nonoArray[MAX][MAX];
+        static int rijen[MAX][MAX];
+        static int hoogte, breedte;
+
 };
 int main(){
     nonogram::hoofdMenu();
@@ -148,16 +159,30 @@ void nonogram::hoofdMenu(){
         }
     }
 }
-/*
-int countf(){
-    while(isdigit(letter)){
-        //Indien meer iteraties, vermenigvuldig met 10
-        sum *= sum > 0 ? 10 : sum;
-        sum += (letter - '0');
-        letter = invoer.get();
-    }
-    //Wijs de waarde 1 toe aan sum als er geen getal achter het
-    //karakter staat
-    return (sum = sum ? sum : 1);
-}*/
+
+
+void nonogram::maakNonoArray(){
+    int i, j;
+    for ( i = 0; i < hoogte; i++ ) {
+        for ( j = 0; j < breedte; j++ ) {
+            nonoArray[i][j] = 0;
+        }//for j
+    }//for i
+} //end maakNonogram
+
+void nonogram::drukAf(){
+    int i, j;
+    for ( i = 0; i < hoogte; i++ ) {
+        for ( j = 0; j < breedte; j++ ) {
+            if ( nonoArray[i][j] )
+                cout << " X";
+            else
+                cout << " O";
+        }//for j
+        cout << endl;
+    }//for i
+}//nonogram::drukaf
+
 int nonogram::rpercent = 50;
+bool nonogram::nonoArray[nonogram::MAX][nonogram::MAX];
+int nonogram::hoogte,nonogram::breedte;
