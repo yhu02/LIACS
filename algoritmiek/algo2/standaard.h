87d2ba1ef8e52f84e030679b79222ec0fe316e57
YvoHu <S262802@vuw.leidenuniv.nl>
2021-04-12 09:36:35 -0700

    standaard

--- algoritmiek/algo2/standaard.h
+++ algoritmiek/algo2/standaard.h
@@ -3,19 +3,38 @@
 #ifndef StandaardHVar  // om te voorkomen dat dit .h bestand meerdere keren
 #define StandaardHVar  // wordt ge-include 
 
-// Controleer of variabele met naam `variabele' een waarde `waarde' heeft
-// die tussen (inclusief) minWaarde en maxWaarde in ligt.
-// Zo nee, geef een passende foutmelding.
-//
-// Voorbeeld van aanroep:
-//   if (integerInBereik ("teller", teller, 0, 1000))
-//     ...
-bool integerInBereik (const char *variabele, int waarde,
-                      int minWaarde, int maxWaarde);
+#include <iostream>
+#include <cstdlib>  // voor rand
+#include "standaard.h"
+using namespace std;
 
-// Genereer een random geheel getal r waarvoor  min <= r <= max.
-// Pre: min <= max;
-int randomGetal (int min, int max);
+//*************************************************************************
+
+bool integerInBereik (const char *variabele, int waarde,
+                      int minWaarde, int maxWaarde)
+{
+  if (waarde>=minWaarde && waarde<=maxWaarde)
+    return true;
+  else
+  { cout << variabele << "=" << waarde << ", maar moet in [" << minWaarde
+         << "," << maxWaarde << "] liggen." << endl;
+    return false;
+  }
+
+}  // integerInBereik
+
+//*************************************************************************
+
+int randomGetal (int min, int max)
+{ int bereik,
+      r;
+
+  bereik = max - min + 1;
+
+  r = ((rand())%bereik) + min;
+  return r;
+
+}  // randomGetal
 
 #endif
 
